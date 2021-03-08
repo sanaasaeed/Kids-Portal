be50dba3e0190421f348f0f43531b8f3564b30a0
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-03-08 21:10:36 +0500

    level setter added

---
+++ Assets/Portal/Kindergarten/Scripts/LevelSetterInPopup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSetterInPopup : MonoBehaviour {
+    [SerializeField] private Text leveltext;
+    private int levelToPlay = 9;
+    void Start() {
+        leveltext.text = "Level " + levelToPlay;
+    }
+
+}
