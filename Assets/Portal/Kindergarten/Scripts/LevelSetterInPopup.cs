9e53148a63948c753f1b8a672ca091ba45987f95
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-03-09 11:10:08 +0500

    rewards homescreen ui added

--- Assets/Portal/Kindergarten/Scripts/LevelSetterInPopup.cs
+++ Assets/Portal/Kindergarten/Scripts/LevelSetterInPopup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class LevelSetterInPopup : MonoBehaviour {
     [SerializeField] private Text leveltext;
     private int levelToPlay = 9;
