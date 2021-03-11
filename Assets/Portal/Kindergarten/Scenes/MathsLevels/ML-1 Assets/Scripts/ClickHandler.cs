353d605f51f1c689250be5c570fc4bb0f9d81911
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-03-11 11:55:26 +0500

    Maths game i1 added

---
+++ Assets/Portal/Kindergarten/Scenes/MathsLevels/ML-1 Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickHandler : MonoBehaviour {
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private TMP_Text buttonText;
+    private RandomSelection randomSelection;
+    void Start() {
+        randomSelection = gameManager.GetComponent<RandomSelection>();
+        HandleClick();
+    }
+
+    public void HandleClick() {
+        int noOfThings = randomSelection.noOfThings;
+        if (buttonText.text == noOfThings.ToString()) {
+            randomSelection.DestroyItems();
+            randomSelection.DisplayThings();
+        }
+    }
+}
