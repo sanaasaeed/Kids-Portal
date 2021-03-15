4296425ae04e6e10da5e16e3225a09610f4535af
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-03-15 12:52:17 +0500

    math lvl 2

---
+++ Assets/Portal/Kindergarten/Scenes/MathsLevels/Shapes/Scripts/Get.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Get : MonoBehaviour {
+    public string name;
+    public string age;
+}
