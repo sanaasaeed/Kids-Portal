f4f76f020e32bbcf26a02e1de6c11ca2ffb6d5dc
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-04-11 00:24:04 +0500

    maths game 2 updated

--- Assets/Portal/Kindergarten/Scenes/MathsLevels/Shapes/Scripts/ObjectSpawner.cs
+++ Assets/Portal/Kindergarten/Scenes/MathsLevels/Shapes/Scripts/ObjectSpawner.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectSpawner : MonoBehaviour {
     [SerializeField] private List<GameObject> outlinedShapePrefab;
     [SerializeField] private List<GameObject> filledShapePrefab;
-    List<float> xpos = new List<float>(){5.5f, 0, 5.5f};
+    List<float> xpos = new List<float>(){-5.5f, 0, 5.5f};
     void Start() {
         float i = -5.5f;
         while (i < 6f) {
@@ -16,9 +18,13 @@ public class ObjectSpawner : MonoBehaviour {
     }
 
     private void SetInitialShapes(float i, int randomNum) {
-        
         int randomXPos = Random.Range(0, xpos.Count);
         Instantiate(outlinedShapePrefab[randomNum], new Vector3(i, 1, -1),Quaternion.identity);
-        Instantiate(filledShapePrefab[randomNum], new Vector3(xpos[Random.Range(0, xpos.Count)], -3.3f, -1),Quaternion.identity);
+        Instantiate(filledShapePrefab[randomNum], new Vector3(xpos[randomXPos], -3.3f, -1),Quaternion.identity);
+        xpos.RemoveAt(randomXPos);
+    }
+
+    private void Drag() {
+        
     }
 }
---
+++ Assets/Portal/Kindergarten/Scenes/MathsLevels/Shapes/Scripts/Shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+    private Transform shapePos;
+    private Vector2 initalPos;
+    private Vector2 mousePos;
+    private float deltaX, deltaY;
+    public static bool locked;
+
+    private void Start() {
+        initalPos = transform.position;
+    }
+
+    private void OnMouseDown() {
+        if (!locked) {
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag() {
+        if (!locked) {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePos.x - deltaX, mousePos.y - deltaY, -1);
+        }
+    }
+
+    //private void OnMouseUp(Collider other) {
+      //  if (Mathf.Abs(transform.position.x - other.transform.position.x) <= 0.5f &&
+      //      Mathf.Abs(transform.position.y - other.transform.position.y) <= 0.5f) {
+      //      transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -1);
+      //  }
+   // }
+
+    /*private void OnTriggerEnter(Collider other) {
+        if (Input.GetMouseButtonUp(0)) {
+            Debug.Log("Entered");
+            if (Mathf.Abs(transform.position.x - other.transform.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - other.transform.position.y) <= 0.5f) {
+                        transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -1);
+            }
+        }
+    }*/
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (Input.GetMouseButtonUp(0)) {
+            if (gameObject.CompareTag(other.tag)) {
+                /*if (Mathf.Abs(transform.position.x - other.transform.position.x) <= 3f &&
+                    Mathf.Abs(transform.position.y - other.transform.position.y) <= 3f) {*/
+                    transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -1);
+                //}
+            }
+            else {
+                transform.position = new Vector3(initalPos.x, initalPos.y, -1);
+            }
+        }
+    }
+}
