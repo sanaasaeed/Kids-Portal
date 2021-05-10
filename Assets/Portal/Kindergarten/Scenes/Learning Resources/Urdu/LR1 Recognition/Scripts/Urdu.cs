116a653086c9a5526b2543312040f3f722197c2f
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-11 01:44:03 +0500

    ULR-1: Initial setup done

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/Urdu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Urdu : MonoBehaviour {
+    [SerializeField] private List<Sprite> urduAlphabetsSprite;
+    [SerializeField] private GameObject alphabetContainer;
+    private GameObject clone;
+
+    private void Start() {
+        clone =  Instantiate(alphabetContainer, new Vector3(0,0,-1), Quaternion.identity);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Dpace pressed");
+            
+            Destroy(clone);
+            new WaitForSeconds(3);
+            Instantiate(alphabetContainer, new Vector3(0,0,-1), Quaternion.identity);
+        }
+    }
+}
