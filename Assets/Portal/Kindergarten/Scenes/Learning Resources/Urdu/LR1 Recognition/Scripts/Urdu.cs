8e7f2773e8ddf4617b5c82d0f5448bc5f153ac8d
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-12 21:05:41 +0500

    ULR-1: Repeat Btn Done

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/Urdu.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/Urdu.cs
@@ -20,7 +20,6 @@ public class Urdu : MonoBehaviour {
     }
 
     public void NextAlphabet() {
-            Debug.Log("Space pressed");
             Destroy(clone);
             for (int i = 0; i < 3; i++) {
                 Debug.Log("Alphabet No: " + alphabetNo);
@@ -30,6 +29,15 @@ public class Urdu : MonoBehaviour {
             clone = Instantiate(alphabetContainer, new Vector3(0,0,-1), Quaternion.identity); 
         }
 
+    public void RepeatAlphabet() {
+        Destroy(clone);
+        int newAlphabetNo = alphabetNo - 2;
+        for (int i = 0; i < 3; i++) {
+            SetChild(i, newAlphabetNo);
+            newAlphabetNo++;
+        }
+        clone = Instantiate(alphabetContainer, new Vector3(0,0,-1), Quaternion.identity); 
+    }
     public void SetChild(int childNo, int alphabetNo) {
         alphabetContainer.transform.GetChild(childNo).GetComponent<SpriteRenderer>().sprite = urduAlphabetsSprite[alphabetNo];
     }
