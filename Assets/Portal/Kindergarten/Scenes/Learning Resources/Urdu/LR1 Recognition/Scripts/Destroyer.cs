84ee6e597e48cae235aa909a2d0fb37447d2096a
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-14 03:06:14 +0500

    ULR-1: Destroying in activity

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/Destroyer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnMouseDown() {
+        if (gameObject.GetComponent<SpriteRenderer>().sprite == ULR1Activity.nextAlphabet) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/ULR1Activity.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Urdu/LR1 Recognition/Scripts/ULR1Activity.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Random = UnityEngine.Random;
 
 public class ULR1Activity : MonoBehaviour {
     [SerializeField] private List<Sprite> alphabetList;
     [SerializeField] private GameObject tarHarf;
     [SerializeField] private List<GameObject> otherHarfs;
+    public static Sprite nextAlphabet;
     void Start() {
         var index = Random.Range(ULR1.alphabetNo, ULR1.alphabetNo - 9);
         var targetAlphabet = alphabetList[index];
-        var nextAlphabet = alphabetList[index + 1];
+        nextAlphabet = alphabetList[index + 1];
         tarHarf.GetComponent<SpriteRenderer>().sprite = targetAlphabet;
         otherHarfs[Random.Range(0, otherHarfs.Count)].GetComponent<SpriteRenderer>().sprite = nextAlphabet;
         Debug.Log(targetAlphabet);
@@ -22,10 +21,6 @@ public class ULR1Activity : MonoBehaviour {
                 otherHarf.GetComponent<SpriteRenderer>().sprite = alphabetList[Random.Range(0, alphabetList.Count)];
             }
         }
-    }
-
-    private void OnMouseDown() {
-        
     }
 
     public void BackBtn() {
