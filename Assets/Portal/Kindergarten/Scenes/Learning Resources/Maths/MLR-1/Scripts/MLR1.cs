ee2d08770c581179a5a2d99b733b8c6cdb97d5da
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-16 18:32:18 +0500

    MLR-1: Sunflower prefab working

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MLR1 : MonoBehaviour {
+    [SerializeField] private List<Sprite> mathLetters;
+    [SerializeField] private GameObject letterPrefab;
+    [SerializeField] private GameObject sunflowerPrefab;
+    void Start() {
+        letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[0];
+        for (int i = 0; i < Int32.Parse(mathLetters[0].name); i++) {
+            Instantiate(sunflowerPrefab, new Vector3(0, -2, -1), quaternion.identity);
+        }
+    }
+
+    public void NextBtn() {
+        
+    }
+    void Update() {
+        
+    }
+}
