ae21fd8ae36bcdca35d59defba451d16fa73f937
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-16 20:25:03 +0500

    MLR-1: Plan changed

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
@@ -9,24 +9,22 @@ public class MLR1 : MonoBehaviour {
     [SerializeField] private List<Sprite> mathLetters;
     [SerializeField] private List<AudioClip> audios;
     [SerializeField] private GameObject letterPrefab;
-    [SerializeField] private GameObject sunflowerPrefab;
     private int letterNo = 0;
-    private GameObject clone;
     private AudioSource letterAudioSource;
     void Start() {
         letterAudioSource = letterPrefab.GetComponent<AudioSource>();
         letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo];
         letterAudioSource.PlayOneShot(audios[letterNo]);
-        for (int i = 0; i < Int32.Parse(mathLetters[0].name); i++) {
+        /*for (int i = 0; i < Int32.Parse(mathLetters[0].name); i++) {
             clone = Instantiate(sunflowerPrefab, new Vector3(0, -2, -1), quaternion.identity);
-        }
+        }*/
         letterNo++;
     }
 
     public void NextBtn() {
         letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo];
         letterAudioSource.PlayOneShot(audios[letterNo]);
-        var allClones = FindObjectsOfType<GameObject>();
+        /*var allClones = FindObjectsOfType<GameObject>();
         foreach (var allClone in allClones) {
             if (allClone.name.Contains("Clone")) {
                 Destroy(allClone);
@@ -34,14 +32,14 @@ public class MLR1 : MonoBehaviour {
         }
         for (int i = 0; i < Int32.Parse(mathLetters[letterNo].name); i++) {
             Instantiate(sunflowerPrefab, new Vector3(0+ i, -2, -1), quaternion.identity);
-        }
+        }*/
         letterNo++;
     }
 
     public void RepeatBtn() {
         letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo-1];
         letterAudioSource.PlayOneShot(audios[letterNo-1]);
-        var allClones = FindObjectsOfType<GameObject>();
+        /*var allClones = FindObjectsOfType<GameObject>();
         foreach (var allClone in allClones) {
             if (allClone.name.Contains("Clone")) {
                 Destroy(allClone);
@@ -49,6 +47,6 @@ public class MLR1 : MonoBehaviour {
         }
         for (int i = 0; i < Int32.Parse(mathLetters[letterNo-1].name); i++) {
             Instantiate(sunflowerPrefab, new Vector3(0+ i, -2, -1), quaternion.identity);
-        }
+        }*/
     }
 }
