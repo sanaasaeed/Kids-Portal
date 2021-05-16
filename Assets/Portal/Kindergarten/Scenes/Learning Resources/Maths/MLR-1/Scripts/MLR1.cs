4e92e042f24405858ea027dcacaed0f683a8c6e0
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-16 20:07:17 +0500

    MLR-1: next btn working

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class MLR1 : MonoBehaviour {
     [SerializeField] private List<Sprite> mathLetters;
+    [SerializeField] private List<AudioClip> audios;
     [SerializeField] private GameObject letterPrefab;
     [SerializeField] private GameObject sunflowerPrefab;
+    private int letterNo = 0;
+    private GameObject clone;
     void Start() {
-        letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[0];
+        letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo];
         for (int i = 0; i < Int32.Parse(mathLetters[0].name); i++) {
-            Instantiate(sunflowerPrefab, new Vector3(0, -2, -1), quaternion.identity);
+            clone = Instantiate(sunflowerPrefab, new Vector3(0, -2, -1), quaternion.identity);
         }
+        letterNo++;
     }
 
     public void NextBtn() {
-        
-    }
-    void Update() {
-        
+        letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo];
+        var allClones = FindObjectsOfType<GameObject>();
+        foreach (var allClone in allClones) {
+            if (allClone.name.Contains("Clone")) {
+                Destroy(allClone);
+            }
+        }
+        for (int i = 0; i < Int32.Parse(mathLetters[letterNo].name); i++) {
+            Instantiate(sunflowerPrefab, new Vector3(0+ i, -2, -1), quaternion.identity);
+        }
+        letterNo++;
     }
 }
