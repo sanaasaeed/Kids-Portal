5baaa2477f803d85d1d12ccc9c7c209a6343e8bb
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-16 22:19:53 +0500

    MLR-1: Added images and audios

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-1/Scripts/MLR1.cs
@@ -24,7 +24,7 @@ public class MLR1 : MonoBehaviour {
         letterPrefab.GetComponent<SpriteRenderer>().sprite = mathLetters[letterNo];
         letterAudioSource.PlayOneShot(audios[letterNo]);
         if (screenNo % 5 == 0) {
-            SceneManager.LoadScene("MLR1_Activity");
+           // SceneManager.LoadScene("MLR1_Activity");
         }
         /*var allClones = FindObjectsOfType<GameObject>();
         foreach (var allClone in allClones) {
