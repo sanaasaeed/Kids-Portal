d7353d8ef3dd28b02b4837ec44e464059fe0e2bb
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-17 23:12:22 +0500

    MLR-2: Setup completed

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/MLR2.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/MLR2.cs
@@ -9,17 +9,22 @@ public class MLR2 : MonoBehaviour {
     [SerializeField] private GameObject digit;
     [SerializeField] private List<Sprite> numbers;
     public static int letterNo = 0;
+    private float lastEggPos = -6.8f;
     private GameObject clone;
+    private int screenNo = 0;
     void Start() {
         digit.GetComponent<SpriteRenderer>().sprite = numbers[letterNo];
         Debug.Log(numbers[letterNo]);
         for (int i = 0; i < Int32.Parse(numbers[letterNo].name); i++) {
-            Instantiate(egg, new Vector3(0+ (i+1), -2, -1), quaternion.identity);
+            Instantiate(egg, new Vector3(lastEggPos, -3.5f, -1), quaternion.identity);
         }
         letterNo++;
     }
 
     public void NextBtn() {
+        screenNo++;
+        if (screenNo % 3 == 0){
+        }
         var allClones = FindObjectsOfType<GameObject>();
         foreach (var allClone in allClones) {
             if (allClone.name.Contains("Clone")) {
@@ -27,8 +32,11 @@ public class MLR2 : MonoBehaviour {
             }
         }
         digit.GetComponent<SpriteRenderer>().sprite = numbers[letterNo];
+        lastEggPos = -6.8f;
         for (int i = 0; i < Int32.Parse(numbers[letterNo].name); i++) {
-            Instantiate(egg, new Vector3(-1 + (i+1), -2, -1), quaternion.identity);
+            Instantiate(egg, new Vector3(lastEggPos, -3.5f, -1), quaternion.identity);
+            Debug.Log("Last Egg Position: " + lastEggPos);
+            lastEggPos = lastEggPos + 1.5f;
         }
         letterNo++;
     }
