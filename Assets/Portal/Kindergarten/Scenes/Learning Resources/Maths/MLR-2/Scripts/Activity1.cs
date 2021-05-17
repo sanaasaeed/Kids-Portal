8a7ae06402c356c91724de8eeaeee7eaad3336bd
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-18 02:27:43 +0500

    MLR-2/A1: Randomnesss done

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Activity1 : MonoBehaviour {
     [SerializeField] private List<Sprite> numbers;
     [SerializeField] private List<Sprite> objects;
+    [SerializeField] private List<GameObject> digitPrefabs;
+    private void Start() {
+        foreach (var digitPrefab in digitPrefabs) {
+            int randomIndex = Random.Range(0, numbers.Count);
+            digitPrefab.GetComponent<SpriteRenderer>().sprite = numbers[randomIndex];
+            
+            numbers.Remove(numbers[randomIndex]);
+        }
+    }
 }
