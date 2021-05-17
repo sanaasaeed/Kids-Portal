dba98318ed01d0eb7640279281169a5b991f13da
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-18 02:06:42 +0500

    MLR-2/A1: Initial setup done

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Activity1 : MonoBehaviour {
-    
+    [SerializeField] private List<Sprite> numbers;
+    [SerializeField] private List<Sprite> objects;
 }
