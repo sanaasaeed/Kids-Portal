a648ba8a1bc472205a79904dbfb944eb6bc35dfa
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-17 18:21:04 +0500

    MLR-2: Something's not working but initial

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/MLR2.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/MLR2.cs
@@ -1,12 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class MLR2 : MonoBehaviour {
     [SerializeField] private GameObject egg;
     [SerializeField] private GameObject digit;
     [SerializeField] private List<Sprite> numbers;
+    public static int letterNo = 0;
+    private GameObject clone;
     void Start() {
-        
+        digit.GetComponent<SpriteRenderer>().sprite = numbers[letterNo];
+        Debug.Log(numbers[letterNo]);
+        for (int i = 0; i < Int32.Parse(numbers[letterNo].name); i++) {
+            Instantiate(egg, new Vector3(0+ (i+1), -2, -1), quaternion.identity);
+        }
+        letterNo++;
     }
+
+    public void NextBtn() {
+        var allClones = FindObjectsOfType<GameObject>();
+        foreach (var allClone in allClones) {
+            if (allClone.name.Contains("Clone")) {
+                Destroy(allClone);
+            }
+        }
+        digit.GetComponent<SpriteRenderer>().sprite = numbers[letterNo];
+        for (int i = 0; i < Int32.Parse(numbers[letterNo].name); i++) {
+            Instantiate(egg, new Vector3(-1 + (i+1), -2, -1), quaternion.identity);
+        }
+        letterNo++;
+    }
+    
 }
