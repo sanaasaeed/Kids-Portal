b28e110f954418f4bfaca3f28402fb9596da3794
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-17 16:20:09 +0500

    MLR-1: necessary fields serializied

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/MLR2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLR2 : MonoBehaviour {
+    [SerializeField] private GameObject egg;
+    [SerializeField] private GameObject digit;
+    [SerializeField] private List<Sprite> numbers;
+    void Start() {
+        
+    }
+}
