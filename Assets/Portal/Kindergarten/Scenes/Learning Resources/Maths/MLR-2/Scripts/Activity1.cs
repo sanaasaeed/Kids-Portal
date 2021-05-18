111225c1223d6fabf3d670a8b5affc24850d82a6
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-18 13:08:54 +0500

    MLR-2/A1: Random objects done

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
@@ -7,13 +7,22 @@ using Random = UnityEngine.Random;
 public class Activity1 : MonoBehaviour {
     [SerializeField] private List<Sprite> numbers;
     [SerializeField] private List<Sprite> objects;
+    [SerializeField] private List<GameObject> objPrefabs;
     [SerializeField] private List<GameObject> digitPrefabs;
+    private Sprite currentNo;
     private void Start() {
         foreach (var digitPrefab in digitPrefabs) {
             int randomIndex = Random.Range(0, numbers.Count);
+            currentNo = numbers[randomIndex];
             digitPrefab.GetComponent<SpriteRenderer>().sprite = numbers[randomIndex];
-            
             numbers.Remove(numbers[randomIndex]);
+            foreach (var objPrefab in objPrefabs) {
+                Debug.Log(objPrefab.name + " Object prefab");
+                Debug.Log(currentNo.name + " Current name");
+                if (objPrefab.name == currentNo.name) {
+                    Instantiate(objPrefab);
+                }
+            }
         }
     }
 }
