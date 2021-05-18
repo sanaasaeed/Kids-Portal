db33f581761283679ad69ca3cebeabfac02092dc
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-18 19:35:21 +0500

    MLR-2/A1: Random index solved

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-2/Scripts/Activity1.cs
@@ -10,22 +10,24 @@ public class Activity1 : MonoBehaviour {
     [SerializeField] private List<GameObject> objPrefabs;
     [SerializeField] private List<GameObject> digitPrefabs;
     private Sprite currentNo;
+    private List<float> yPositions;
     private void Start() {
+        yPositions = new List<float>(){2f, 0f, -2.2f, -4.5f};
         foreach (var digitPrefab in digitPrefabs) {
             int randomIndex = Random.Range(0, numbers.Count);
             currentNo = numbers[randomIndex];
             digitPrefab.GetComponent<SpriteRenderer>().sprite = numbers[randomIndex];
             numbers.Remove(numbers[randomIndex]);
-            List<float> yPositions = new List<float>(){2f, 0f, -2.2f, -4.5f};
-            int randomYIndex = Random.Range(0, yPositions.Count);
+            int randomYIndex = Random.Range(0, yPositions.Count); 
+            Debug.Log("Random Y index: " + randomYIndex);
             foreach (var objPrefab in objPrefabs) {
-                
                 if (objPrefab.name == currentNo.name) {
                     Instantiate(objPrefab, new Vector3(0, yPositions[randomYIndex], 0), Quaternion.identity);
                 }
             }
-            yPositions.RemoveAt(randomYIndex);
-
+            yPositions.Remove(yPositions[randomYIndex]);
         }
+       
+
     }
 }
