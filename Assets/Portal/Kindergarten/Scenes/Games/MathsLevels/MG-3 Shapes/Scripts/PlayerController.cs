8f6a91924b151f1d9b8cfcb374286d273c6bf0b7
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-19 23:28:16 +0500

    MG-3: Player added

---
+++ Assets/Portal/Kindergarten/Scenes/Games/MathsLevels/MG-3 Shapes/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 5f;
+
+    private void Update() {
+        MovePlayer();
+    }
+
+    void MovePlayer() {
+        if (Input.GetAxisRaw("Vertical") > 0f) {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+            transform.position = temp;
+        }else if (Input.GetAxisRaw("Vertical") < 0f) {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+} // class
