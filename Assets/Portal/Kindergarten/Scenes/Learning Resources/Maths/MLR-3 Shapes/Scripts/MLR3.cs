0333cf505a03cdd8382df0e23c7ffe571257b484
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-19 11:28:40 +0500

    MLR-3: Next Re[eat btn working

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-3 Shapes/Scripts/MLR3.cs
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-3 Shapes/Scripts/MLR3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,15 +11,36 @@ public class MLR3 : MonoBehaviour {
     [SerializeField] private List<GameObject> shapesPrefab;
     [SerializeField] private TextMeshProUGUI shapeText;
     [SerializeField] private List<Sprite> shapes;
+    public static int randomNextIndex;
 
     private void Start() {
         int randomIndex = Random.Range(0, shapesPrefab.Count);
+        SetShape(randomIndex);
+    }
+    
+    public void NextBtn() {
+        RemoveClones();
+        randomNextIndex = Random.Range(0, shapesPrefab.Count);
+        SetShape(randomNextIndex);
+    }
+
+    public void RepeatBtn() {
+        RemoveClones();
+        SetShape(randomNextIndex);
+    }
+
+    public void SetShape(int randomIndex) {
         GameObject currentPrefab = shapesPrefab[randomIndex];
         Instantiate(currentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         shapeText.text = currentPrefab.name;
     }
-    
-    public void NextBtn(){
-    
+
+    void RemoveClones() {
+        var allClones = FindObjectsOfType<GameObject>();
+        foreach (var clone in allClones) {
+            if (clone.name.Contains("Clone")) {
+                Destroy(clone);
+            }
         }
+    }
 }
