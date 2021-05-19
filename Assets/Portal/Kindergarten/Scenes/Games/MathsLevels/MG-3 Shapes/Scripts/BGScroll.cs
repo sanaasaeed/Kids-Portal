8a0218c8c7940685bc0f8f0dc801c7ef89e1dbc8
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-19 23:18:53 +0500

    MG-3: Background infinite scroll done

---
+++ Assets/Portal/Kindergarten/Scenes/Games/MathsLevels/MG-3 Shapes/Scripts/BGScroll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+   public float scrollSpeed = 0.1f;
+   private MeshRenderer meshRenderer;
+   private float xScroll;
+
+   private void Awake() {
+      meshRenderer = GetComponent<MeshRenderer>();
+   }
+
+   private void Update() {
+      Scroll();
+   }
+
+   void Scroll() {
+      xScroll = Time.time * scrollSpeed;
+      Vector2 offset = new Vector2(xScroll, 0f);
+      meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+   }
+}
