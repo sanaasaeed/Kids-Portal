5c3899afef3832bb1084f2f7328b946a3b109e9f
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-19 11:16:49 +0500

    MLR-3: Start setup done

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/Maths/MLR-3 Shapes/Scripts/MLR3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class MLR3 : MonoBehaviour {
+    [SerializeField] private List<GameObject> shapesPrefab;
+    [SerializeField] private TextMeshProUGUI shapeText;
+    [SerializeField] private List<Sprite> shapes;
+
+    private void Start() {
+        int randomIndex = Random.Range(0, shapesPrefab.Count);
+        GameObject currentPrefab = shapesPrefab[randomIndex];
+        Instantiate(currentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        shapeText.text = currentPrefab.name;
+    }
+    
+}
