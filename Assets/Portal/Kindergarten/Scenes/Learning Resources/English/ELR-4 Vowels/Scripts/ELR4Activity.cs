c4643c591da8dbecadf1e18f171bc94587e8ad31
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-21 01:27:33 +0500

    ELR-4: Fields Serialized

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/English/ELR-4 Vowels/Scripts/ELR4Activity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELR4Activity : MonoBehaviour {
+    [SerializeField] private List<Sprite> consonants;
+    [SerializeField] private List<Sprite> vowels;
+
+    private void Start() {
+        
+    }
+}
