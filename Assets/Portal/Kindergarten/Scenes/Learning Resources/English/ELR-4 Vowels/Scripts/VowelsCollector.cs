c2864f50b4254a8638b5a261c3ad9e4ab2ba963a
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-21 02:41:55 +0500

    ELR-4: Vowels collector done

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/English/ELR-4 Vowels/Scripts/VowelsCollector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VowelsCollector : MonoBehaviour
+{
+    private void OnMouseDown() {
+        if (gameObject.GetComponent<SpriteRenderer>().sprite.name.Contains("A") ||
+            gameObject.GetComponent<SpriteRenderer>().sprite.name.Contains("E") ||
+            gameObject.GetComponent<SpriteRenderer>().sprite.name.Contains("I") ||
+            gameObject.GetComponent<SpriteRenderer>().sprite.name.Contains("O") ||
+            gameObject.GetComponent<SpriteRenderer>().sprite.name.Contains("U")) {
+            Destroy(gameObject);
+        }
+        else {
+            // TODO: else logic
+        }
+    }
+}
