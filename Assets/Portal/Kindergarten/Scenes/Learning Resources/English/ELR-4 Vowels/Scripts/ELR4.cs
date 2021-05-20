af558a0c40f27306f72db3d2d88afb0fb660c06f
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-20 15:11:58 +0500

    ELR-4: Other things added

---
+++ Assets/Portal/Kindergarten/Scenes/Learning Resources/English/ELR-4 Vowels/Scripts/ELR4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ELR4 : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI panelText;
+    [SerializeField] private GameObject boardSet;
+    [SerializeField] private List<String> texts;
+    [SerializeField] private List<Sprite> boardImages;
+    private int screenIndex = 0;
+    private void Start() {
+        panelText.text = texts[screenIndex];
+        boardSet.GetComponent<SpriteRenderer>().sprite = boardImages[screenIndex];
+        screenIndex++;
+        StartCoroutine(Wait(2f));
+    }
+
+    IEnumerator Wait(float noOfSeconds) {
+        yield return new WaitForSeconds(noOfSeconds);
+        ChangeScene();
+    }
+
+    void ChangeScene() {
+        if (screenIndex < texts.Count && screenIndex < boardImages.Count) {
+            panelText.text = texts[screenIndex];
+            boardSet.GetComponent<SpriteRenderer>().sprite = boardImages[screenIndex];
+            screenIndex++;
+            StartCoroutine(Wait(3f));
+        }
+        else {
+            // TODO: Win screen
+        }
+    }
+}
