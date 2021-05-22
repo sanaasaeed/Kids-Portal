5ab5794ca1837c07f72a82207b5dbd9eb1cc56bd
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-22 13:58:57 +0500

    MG-4: Bullet firing done

--- Assets/Portal/Kindergarten/Scenes/Games/EnglishLevels/EG-4 Vowels/Scripts/CharacterController.cs
+++ Assets/Portal/Kindergarten/Scenes/Games/EnglishLevels/EG-4 Vowels/Scripts/CharacterController.cs
@@ -9,15 +9,27 @@ public class CharacterController : MonoBehaviour {
     private float movement;
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform attackPoint;
+    private float minX = -8f;
+    private float maxX = 1.5f;
     private void Update() {
         movement = Input.GetAxisRaw("Horizontal") * speed;
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            //Instantiate()
+            Instantiate(bullet, attackPoint.position, Quaternion.Euler(0f,0f, 90));
         }
     }
 
     private void FixedUpdate() {
+        Vector3 temp = transform.position;
         rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+        if (temp.x > maxX) {
+            temp.x = maxX;
+        }
+        transform.position = temp;
+        
+        if (temp.x < minX) {
+            temp.x = minX;
+        }
+        transform.position = temp;
     }
 }
