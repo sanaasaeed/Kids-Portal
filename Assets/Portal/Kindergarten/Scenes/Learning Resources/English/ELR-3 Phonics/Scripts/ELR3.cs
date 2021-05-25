68a33589930021c783362a6515e41f0a16c0a174
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-05-25 13:55:13 +0500

    Levels container changed

--- Assets/Portal/Kindergarten/Scenes/Learning Resources/English/ELR-3 Phonics/Scripts/ELR3.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ELR3 : MonoBehaviour {
-    [SerializeField] private List<string> texts;
-    [SerializeField] private List<Sprite> images;
-    
-}
